6f7bbde87d09b027d1094b4674031e7ad4855952
midnight <kevinrodriguez4851@gmail.com>
2023-01-09 15:24:09 -0600

    config refactor

--- lua/config.lua
+++ lua/config.lua
@@ -1,9 +1,14 @@
+local plg = require("plug")
 local key = require("keymaps")
 local env = require("environment")
 
 local setup = {
-  key.setup(),
-  env.setup()
+  plg.setup(), -- load plugins
+  key.setup(), -- load keymaps
+  env.setup(), -- load environment
+
+  -- set colorscheme
+  vim.cmd("colorscheme starry_nebula")
 }
 
 return setup
