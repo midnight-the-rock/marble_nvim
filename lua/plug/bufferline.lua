beee922fd212d89b955d3b7b9eb8387922caa720
midnight <kevinrodriguez4851@gmail.com>
2023-01-09 21:45:56 -0600

    refactor

--- lua/plug/bufferline.lua
+++ lua/plug/bufferline.lua
@@ -1,59 +1,51 @@
 local bufferline = require("bufferline")
 
-bufferline.setup
+local M = {}
+
+M.options =
 {
-  options =
-  {
-    mode = "buffers", -- set to "tabs" to only show tabpages instead
-    numbers = "none",
-    close_command = "bdelete! %d",       -- can be a string | function, see "Mouse actions"
-    right_mouse_command = "bdelete! %d", -- can be a string | function, see "Mouse actions"
-    left_mouse_command = "buffer %d",    -- can be a string | function, see "Mouse actions"
-    middle_mouse_command = nil,          -- can be a string | function, see "Mouse actions"
-    indicator =
-    {
-        style = false,
-    },
-    buffer_close_icon = '',
-    modified_icon = '●',
-    close_icon = '',
-    left_trunc_marker = '',
-    right_trunc_marker = '',
-    name_formatter = function(buf)  -- buf contains:
-      return "/ "..buf.name.." /"
-    end,
-    max_name_length = 18,
-    max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
-    truncate_names = true, -- whether or not tab names should be truncated
-    tab_size = 18,
-    diagnostics = "nvim_lsp",
-    diagnostics_update_in_insert = false,
-    diagnostics_indicator = false,
-    offsets =
-    {
-        {
-            filetype = "NvimTree",
-            text = "File Explorer",
-            text_align = "center",
-            separator = true
-        }
-    },
-    color_icons = true, -- whether or not to add the filetype icon highlights
-    show_buffer_icons = true, -- disable filetype icons for buffers
-    show_buffer_close_icons = true,
-    show_buffer_default_icon = true, -- whether or not an unrecognised filetype should show a default icon
-    show_close_icon = true,
-    show_tab_indicators = false,
-    show_duplicate_prefix = true, -- whether to show duplicate buffer prefix
-    persist_buffer_sort = true, -- whether or not custom sorted buffers should persist
-    separator_style = { ' ▎', ' ▎' },
-    enforce_regular_tabs = false,
-    always_show_bufferline = true,
-    hover = {
-        enabled = true,
-        delay = 200,
-        reveal = {'close'}
-    },
-    sort_by = 'insert_at_end',
-  },
+  mode = "buffers",
+  numbers = "none",
+  middle_mouse_command = nil,
+  close_command = "bdelete! %d",
+  left_mouse_command = "buffer %d",
+  right_mouse_command = "bdelete! %d",
+  indicator = { style = "none" },
+  close_icon = '',
+  modified_icon = '',
+  buffer_close_icon = '',
+  left_trunc_marker = '',
+  right_trunc_marker = '',
+  name_formatter = function(buf)
+    return "/ "..buf.name.." /"
+  end,
+  max_name_length = 18,
+  max_prefix_length = 15,
+  truncate_names = true,
+  tab_size = 18,
+  diagnostics = "nvim_lsp",
+  diagnostics_indicator = false,
+  diagnostics_update_in_insert = false,
+  offsets =
+  {{
+    filetype = "NvimTree",
+    text = " /NvimTree",
+    text_align = "center",
+    separator = true
+  }},
+  color_icons = true,
+  show_buffer_icons = true,
+  show_buffer_close_icons = true,
+  show_buffer_default_icon = true,
+  show_close_icon = true,
+  show_tab_indicators = false,
+  show_duplicate_prefix = true,
+  persist_buffer_sort = true,
+  separator_style = "slant",
+  enforce_regular_tabs = false,
+  always_show_bufferline = true,
+  hover = { enabled = true, delay = 200, reveal = {'close'} },
+  sort_by = 'insert_at_end',
 }
+
+return bufferline.setup(M)
