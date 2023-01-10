a238b7302c2141febb3665f382b125012cc79960
midnight <kevinrodriguez4851@gmail.com>
2023-01-09 21:17:38 -0600

    refactor

--- lua/plug/nvimtree.lua
+++ lua/plug/nvimtree.lua
@@ -1,51 +1,48 @@
 local nvim_tree = require("nvim-tree")
 
-nvim_tree.setup
+local M = {}
+
+M.update_focused_file = { enable = true, update_cwd = true, }
+
+M.renderer = 
 {
-  update_focused_file =
-  {
-    enable = true,
-    update_cwd = true,
-  },
-  renderer =
-  {
-    root_folder_modifier = ":t",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_open = "",
-          arrow_closed = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "U",
-          deleted = "",
-          ignored = "◌",
-        },
+  root_folder_modifier = ":t",
+  icons = {
+    glyphs = 
+    {
+      default = "",
+      symlink = "",
+      folder = 
+      {
+        arrow_open = "",
+        arrow_closed = "",
+        default = "",
+        open = "",
+        empty = "",
+        empty_open = "",
+        symlink = "",
+        symlink_open = "",
+      },
+      git = 
+      {
+        unstaged = "",
+        staged = "",
+        unmerged = "",
+        renamed = "➜",
+        untracked = "",
+        deleted = "",
+        ignored = "◌",
       },
     },
-  },
-  diagnostics =
-  {
-    enable = true,
-    show_on_dirs = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-
+  }
 }
+
+M.diagnostics =
+{
+  enable = true,
+  show_on_dirs = true,
+  icons = { hint = "", info = "", warning = "", error = "" }
+}
+
+
+return nvim_tree.setup(M)
