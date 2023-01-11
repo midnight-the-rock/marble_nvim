14183bd846ad20ff7b4e0c3c2a244a83d08eebf5
midnight <kevinrodriguez4851@gmail.com>
2023-01-10 21:17:42 -0600

    refactor: install script breaks installation

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #! /bin/bash
 
-# install packer if missing 
-if [ -e ~/.local/share/nvim/site/pack/packer ]; then
-  echo ~/.local/share/nvim/site/pack/packer
-else
-  git clone --depth 1 https://github.com/wbthomason/packer.nvim\
-   ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-fi
-
 # check for older nvim configs
 if [ -e ~/.config/nvim ]; then
   mv ~/.config/nvim ~/nvim-old
@@ -16,3 +8,7 @@ fi
 # install configs
 git clone --depth 1 https://github.com/TheMidnightShow/starry.nvim\
  ~/.config/nvim
+
+clear
+
+echo -e "\nstarry.nvim installed!"
