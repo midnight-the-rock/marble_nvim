e699388b100b84853e9fc0ee056c3386341d241f
midnight <kevinrodriguez4851@gmail.com>
2023-01-11 15:14:20 -0600

    refactor: improved installer

--- install.sh
+++ install.sh
@@ -1,14 +1,51 @@
 #! /bin/bash
 
-# check for older nvim configs
-if [ -e ~/.config/nvim ]; then
-  mv ~/.config/nvim ~/nvim-old
+install()
+{
+  clear
+  echo -e "\n========================================================"
+  echo -e "installing starry.nvim at ~/.config/nvim"
+  echo -e "========================================================\n"
+
+  # check for older nvim configs
+  if [ -e ~/.config/nvim ]; then
+    mv ~/.config/nvim ~/nvim-old
+  fi
+
+  # install starry template 
+  git clone --depth 1 https://github.com/TheMidnightShow/starry.nvim\
+   ~/.config/nvim
+  sleep 0.5s
+
+  clear
+
+  echo -e "\n========================================================"
+  echo -e "starry.nvim installed!"
+  echo -e "========================================================\n"
+}
+
+aborted()
+{
+  clear
+  echo -e "\n========================================================"
+  echo -e "installation aborted!"
+  echo -e "========================================================\n"
+  sleep 0.5s
+}
+
+clear 
+
+echo ""
+echo "██████ ██                              [nvim]"
+echo "██     ███ ████ ██ ██ ███ ██ ███ ██  ██"
+echo "    ██ ██  ██  ███ ███ ██ ███ ██ ██  ██"
+echo "██████ ███ ████ ██ ██     ██     ██████"
+echo "                                    ██"
+
+echo -e "\nproceed with installation? [y/n]: "; read IN
+
+if [ $IN == "n" ] || [ $IN == "no" ]; then
+  aborted 
+else
+  install
 fi
-
-# install configs
-git clone --depth 1 https://github.com/TheMidnightShow/starry.nvim\
- ~/.config/nvim
-
-clear
-
-echo -e "\nstarry.nvim installed!"
